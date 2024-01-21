a8f4548c8eab4944d162cf1802a995d14f57feab
Jonta Sancar <jontasancar@gmail.com>
2024-01-21 17:09:19 -0300

    finalização: leitura -> conversão -> salvamento.
    CSV para MySQL

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 require_once(__DIR__ . "/./auxiliares/funcoes.php");
 require_once(__DIR__ . "/./Handlers/SQL_CRUD.php");
 
+$arquivos = glob(__DIR__ . DIRECTORY_SEPARATOR . "." . DIRECTORY_SEPARATOR . "avaliacoes" . DIRECTORY_SEPARATOR . "*.csv");
+
 $arquivos_lidos = [];
 foreach ($arquivos as $caminho_arquivo) {
   $nome_arquivo = retornaNomeDoArquivo($caminho_arquivo);
@@ -13,9 +15,21 @@ foreach ($arquivos as $caminho_arquivo) {
     
     $csv_em_array = CDVParaArray($arquivo_aberto);
     
-    if($csv_em_array){
+    if($csv_em_array !== false){
       $av_perguntas = $csv_em_array["perguntas"];
       $av_respostas = $csv_em_array["respostas"];
+
+      $resultado_criacao_tabela = criarTabela($nome_arquivo, array_keys($av_perguntas));
+
+      if($resultado_criacao_tabela){
+        $resultado_cadastro_relacoes = cadastraRelacaoPerguntas($nome_arquivo, $av_perguntas);
+        
+        if($resultado_cadastro_relacoes !== false){
+          $resultado_cadastro_respostas = cadastrarRespostas($nome_arquivo, $av_respostas);
+          echo $nome_arquivo . "<br>";
+          var_dump($resultado_cadastro_respostas);
+        }
+      }
     }
     
     $arquivos_lidos[] = $nome_arquivo;
