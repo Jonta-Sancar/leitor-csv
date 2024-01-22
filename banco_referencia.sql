8ec41c660299e697be2774e77b202808a0b5ff9b
Jonta Sancar <jontasancar@gmail.com>
2024-01-22 00:36:21 -0300

    atualização do banco de referência

--- banco_referencia.sql
+++ banco_referencia.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS `respostas_av`;
 CREATE DATABASE IF NOT EXISTS `respostas_av`;
 
 USE `respostas_av`;
@@ -8,5 +7,15 @@ CREATE TABLE `relaciona_perguntas_a_tabela_de_respostas`(
     tabela VARCHAR(200) NOT NULL,
     identificador_pergunta VARCHAR(50) NOT NULL,
     texto_pergunta VARCHAR(500) NOT NULL,
-    pontuacao_pergunta FLOAT DEFAULT NULL
+    pontuacao_pergunta FLOAT DEFAULT NULL,
+    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE `pontuacao_respostas`(
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tabela VARCHAR(200) NOT NULL,
+    email VARCHAR(200) NOT NULL,
+    identificador_pergunta VARCHAR(50) NOT NULL,
+    pontuacao_pergunta FLOAT DEFAULT NULL,
+    data_cadastro DATETIME DEFAULT CURRENT_TIMESTAMP
 );
