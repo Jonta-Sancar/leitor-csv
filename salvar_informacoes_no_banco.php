6103430531b89c666c5edd50bc68f7307bcf4082
Jonta Sancar <jontasancar@gmail.com>
2024-01-21 21:24:50 -0300

    renomeação do arquivo e adição de respostas visuais

---
+++ salvar_informacoes_no_banco.php
@@ -0,0 +1,93 @@
+<?php
+require_once(__DIR__ . "/./auxiliares/funcoes.php");
+require_once(__DIR__ . "/./Handlers/SQL_CRUD.php");
+
+$diretorio_arquivos = __DIR__ . DIRECTORY_SEPARATOR . "." . DIRECTORY_SEPARATOR . "avaliacoes" . DIRECTORY_SEPARATOR;
+$arquivos = glob($diretorio_arquivos . "*.csv");
+
+$arquivos_lidos = [];
+$tudo_certo = true;
+$messages = [];
+
+foreach ($arquivos as $caminho_arquivo) {
+  $nome_arquivo = retornaNomeDoArquivo($caminho_arquivo);
+
+  $tabela_existe = verificarSeTabelaExiste($nome_arquivo);
+
+  if(!$tabela_existe){
+    $arquivo_aberto  = fopen($caminho_arquivo, "r");
+    
+    $csv_em_array = CDVParaArray($arquivo_aberto);
+    
+    if($csv_em_array !== false){
+      $av_perguntas = $csv_em_array["perguntas"];
+      $av_respostas = $csv_em_array["respostas"];
+
+      $resultado_criacao_tabela = criarTabela($nome_arquivo, array_keys($av_perguntas));
+
+      if($resultado_criacao_tabela){
+        $resultado_cadastro_relacoes = cadastraRelacaoPerguntas($nome_arquivo, $av_perguntas);
+        
+        if($resultado_cadastro_relacoes !== false){
+          $resultado_cadastro_respostas = cadastrarRespostas($nome_arquivo, $av_respostas);
+        }
+      }
+    }
+
+    if($tabela_existe && $csv_em_array === false && !$resultado_criacao_tabela && $resultado_cadastro_relacoes === false){
+      $tudo_certo = false;
+    }
+    
+    $arquivos_lidos[] = $nome_arquivo;
+  } else {
+    $tudo_certo = false;
+    $messages[] = "Não foi possível ler o arquivo $nome_arquivo.csv, pois já existe uma tabela com o nome `$nome_arquivo`";
+    break;
+  }
+}
+
+foreach ($arquivos_lidos as $nome_arquivo) {
+  $caminho_arquivo = $diretorio_arquivos . $nome_arquivo . ".csv";
+
+  if(file_exists($caminho_arquivo)) {
+    if(!unlink($caminho_arquivo)){
+      $messages[] = "não foi possível excluir o arquivo $caminho_arquivo";
+      break;
+    }
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Salvamento de Respostas no Banco de Dados</title>
+</head>
+<body>
+  <h3>Arquivos Lidos</h3>
+  <ul>
+    <?php
+      foreach($arquivos_lidos as $nome_arquivo){
+        ?>
+          <li><?= $nome_arquivo ?></li>
+        <?php
+      }
+    ?>
+  </ul>
+
+  <?php
+    if(!$tudo_certo){
+      ?>
+        <div><small><strong>Erros:</strong></small></div>
+      <?php
+
+      foreach($messages as $message){
+        ?>
+          <li><?= $message ?></li>
+        <?php
+      }
+    }
+  ?>
+</body>
+</html>
